0e880837084d8a89782891f38c5d1d051648f083
José <tmdjconstrutora@gmail.com>
2025-08-06 16:38:41 -0300

    Restaurando site com anúncios funcionando

--- firebase-config.js
+++ firebase-config.js
@@ -2,13 +2,13 @@ import { initializeApp, getApps } from "https://www.gstatic.com/firebasejs/9.22.
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCyUp8VGCMU7haCa-7t-aFGyQETj689us",
+  apiKey: "AIzaSyCyuPy8CMUu7haCa-7t-aFGyQfETJ689us",
   authDomain: "tmdj-imoveis.firebaseapp.com",
   projectId: "tmdj-imoveis",
   storageBucket: "tmdj-imoveis.appspot.com",
-  messagingSenderId: "8880033166",
-  appId: "1:8880033166:web:7564d2cf329f3e375649e7",
-  measurementId: "G-M6S28BXPQX"
+  messagingSenderId: "88800336166",
+  appId: "1:88800336166:web:7564d2cf329f3e375649e7",
+  measurementId: "G-M6S28BXP0X"
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
